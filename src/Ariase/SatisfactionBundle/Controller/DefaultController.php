f27e438a39c34176d0cf4caf80b95cb386d85de1
Serge HARDY <shardy@jouve.fr>
2015-10-27 15:31:57 -0700

    Langue en session

--- src/Ariase/SatisfactionBundle/Controller/DefaultController.php
+++ src/Ariase/SatisfactionBundle/Controller/DefaultController.php
@@ -5,6 +5,8 @@ namespace Ariase\SatisfactionBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class DefaultController extends Controller
 {
@@ -14,6 +16,25 @@ class DefaultController extends Controller
      */
     public function indexAction($name)
     {
-        return array('name' => $name);
+        return array('name' => $name,
+            'locale'=>$this->get('session')->get('lang'));
+    }
+
+    /**
+     * @Route("/setlocale/{lang}", name="setlocale")
+     * @Template()
+     */
+    public function setLocaleAction($lang)
+    {
+        $session = $this->get('session');
+
+        $session->set('lang',$lang);
+
+        $url = $this->container->get('request')->headers->get('referer');
+
+        if(!$url)
+            $url="/";
+
+        return new RedirectResponse($url);
     }
 }
