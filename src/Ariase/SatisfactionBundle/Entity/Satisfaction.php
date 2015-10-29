9b5f4fca32514db053a8c485afe0ea42081e9b88
Serge HARDY <shardy@jouve.fr>
2015-10-29 08:41:56 -0700

    Creation entite

---
+++ src/Ariase/SatisfactionBundle/Entity/Satisfaction.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Ariase\SatisfactionBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Satisfaction
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Satisfaction
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="note", type="integer")
+     */
+    private $note;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="commentaire", type="text")
+     */
+    private $commentaire;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set note
+     *
+     * @param integer $note
+     *
+     * @return Satisfaction
+     */
+    public function setNote($note)
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    /**
+     * Get note
+     *
+     * @return integer
+     */
+    public function getNote()
+    {
+        return $this->note;
+    }
+
+    /**
+     * Set commentaire
+     *
+     * @param string $commentaire
+     *
+     * @return Satisfaction
+     */
+    public function setCommentaire($commentaire)
+    {
+        $this->commentaire = $commentaire;
+
+        return $this;
+    }
+
+    /**
+     * Get commentaire
+     *
+     * @return string
+     */
+    public function getCommentaire()
+    {
+        return $this->commentaire;
+    }
+}
